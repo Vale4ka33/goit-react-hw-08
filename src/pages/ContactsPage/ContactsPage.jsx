bc8da75b4945532eb6e18a02676eeb1a25e01d27
Valentine Ukho <petrova.hfhfhf@gmail.com>
2024-07-06 15:54:48 -1000

    fix render contacts in ContactPage

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -1,14 +1,26 @@
-import ContactForm from '../../components/ContactForm/ContactForm';
-import SearchBox from '../../components/SearchBox/SearchBox';
-import ContactList from '../../components/ContactList/ContactList';
-
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import ContactForm from "../../components/ContactForm/ContactForm";
+import SearchBox from "../../components/SearchBox/SearchBox";
+import ContactList from "../../components/ContactList/ContactList";
+import { fetchContacts } from "../../redux/contacts/operations";
+import { selectContacts } from "../../redux/contacts/selectors"; 
 
 const ContactPage = () => {
-  return <div>
-    <ContactForm/>
-    <SearchBox/>
-    <ContactList/>
-  </div>;
+  const dispatch = useDispatch();
+  const contacts = useSelector(selectContacts); 
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
+  return (
+    <div>
+      <ContactForm />
+      <SearchBox />
+      <ContactList contacts={contacts} />
+    </div>
+  );
 };
 
 export default ContactPage;
